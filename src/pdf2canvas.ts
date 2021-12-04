5a102d454f0f2264f2499e67d2a36142064fbb82
hardcorequal <hardcorequal@gmail.com>
2021-12-04 13:38:46 +0200

    refactor: extract from pdf2png -> pdf2canvas for can create and convertor to other image formats

---
+++ src/pdf2canvas.ts
@@ -0,0 +1,70 @@
+import {strict as assert} from "assert";
+import Canvas from "canvas";
+
+export class NodeCanvasFactory {
+  create(width: number, height: number) {
+    assert(width > 0 && height > 0, "Invalid canvas size");
+    const canvas = Canvas.createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    return {
+      canvas,
+      context,
+    };
+  }
+
+  reset(canvasAndContext, width, height) {
+    assert(canvasAndContext.canvas, "Canvas is not specified");
+    assert(width > 0 && height > 0, "Invalid canvas size");
+    canvasAndContext.canvas.width = width;
+    canvasAndContext.canvas.height = height;
+  }
+
+  destroy(canvasAndContext) {
+    assert(canvasAndContext.canvas, "Canvas is not specified");
+
+    // Zeroing the width and height cause Firefox to release graphics
+    // resources immediately, which can greatly reduce memory consumption.
+    canvasAndContext.canvas.width = 0;
+    canvasAndContext.canvas.height = 0;
+    canvasAndContext.canvas = null;
+    canvasAndContext.context = null;
+  }
+}
+
+type ObjectURL = string
+
+async function* pdf2canvas(url: ObjectURL) {
+  const PDFJS = (window as any).pdfjsLib;
+
+  const loadingTask = await PDFJS.getDocument({url});
+
+  const pdfDocument = loadingTask;
+  const numPages = pdfDocument.numPages;
+
+  for (let pageIndex = 1; pageIndex <= numPages; pageIndex++) {
+    const page = await pdfDocument.getPage(pageIndex);
+
+    // Render the page on a Node canvas with 100% scale.
+    const viewport = page.getViewport({scale: 1.0});
+    const canvasFactory = new NodeCanvasFactory();
+    const canvasAndContext = canvasFactory.create(
+      viewport.width,
+      viewport.height
+    );
+    const renderContext = {
+      canvasContext: canvasAndContext.context,
+      viewport,
+      canvasFactory,
+    };
+
+    const renderTask = page.render(renderContext);
+    await renderTask.promise;
+    yield canvasAndContext.canvas;
+  }
+}
+
+export async function* pdf2png(url: ObjectURL) {
+  for await (let canvas of pdf2canvas(url)) {
+    yield canvas.toDataURL('image/png');
+  }
+}
