6745e42f5259a7639eef57460608025c261aad50
hardcorequal <hardcorequal@gmail.com>
2021-12-05 14:20:05 +0200

    feat: add button for download images as zip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import Head from "next/head";
 import {pdf2png} from "../convert/pdf2canvas";
+import JSZip from 'jszip';
+// @ts-ignore
+import JSZipUtils from 'jszip-utils';
 
 
 export const UiFileInputButton: React.FC<{label: string}> = (props) => {
@@ -36,6 +39,9 @@ export const UiFileInputButton: React.FC<{label: string}> = (props) => {
         <button type="button" onClick={onClickHandler}>
           {props.label}
         </button>
+        <button type="button" onClick={() => downloadZip(pngs, 'images', (e) => console.log(Math.round(e * 100)))}>
+          Download Rendered Images
+        </button>
         <input
           multiple={true}
           onChange={onChangeHandler}
@@ -59,3 +65,40 @@ export default function () {
     <UiFileInputButton label="Upload PDF for convert to PNG" />
   </>
 };
+
+
+async function downloadZip(urls: string[], zipName: string, progress?: (e: number) => void) {
+  var zip = new JSZip();
+  var zipFilename = zipName + ".zip";
+
+  const getBinaryContent = (url: string) : Promise<string> => new Promise((resolve, reject) => {
+    JSZipUtils.getBinaryContent(url, function (err: Error | null, data: string) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data)
+      }
+    });
+  });
+
+  let doneCount = 0;
+
+  const addedImages = urls
+    .map(async (url, index) => {
+      const content = await getBinaryContent(url);
+      zip.file(index + '.png', content, {binary:true});
+
+      doneCount++;
+      progress && progress(doneCount);
+    });
+
+  await Promise.all(addedImages);
+
+  zip.generateAsync({type:'blob'}).then(function(content) {
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(content);
+    link.download = zipFilename;
+    console.log( link.href );
+    link.click();
+  });
+}
